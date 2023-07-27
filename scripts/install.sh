8e1c43237f69927dd6bfd9b9a47cc31be5ae1812
Sergio Alen <sergio_alen@hotmail.com>
2023-07-27 16:25:46 +0800

    Add other development pkgs

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-npm install express
-npm install --save-dev gts @types/express
+npm install express \
+    winston
+
+npm install --save-dev \
+    @types/express \
+    gts \
+    ts-node \
+    nodemon \
+    rimraf
 
 npx gts init
